90e0b9e36b4d3003a826cc717f7638518de40557
MarcoBonato2007 <MarcoBonato2007@gmail.com>
2025-09-03 15:13:07 +0200

    removed searchbar cuz bad firestore search

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:quotebook/constants.dart';
 import 'package:quotebook/explore_page.dart';
+import 'package:quotebook/globals.dart';
 import 'package:quotebook/settings_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -12,6 +17,28 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   int currentPageIndex = 0; // 0 means explore, 1 means settings
 
+  /// Gets a list of liked user quotes, this is passed into ExplorePage() in a FutureBuilder()
+  Future<(ErrorCode?, List<String>)> getLikedQuotes() async {
+    final log = Logger("Getting liked quotes");
+
+    ErrorCode? error;
+    List<String> likedQuotes = [];
+
+    await FirebaseFirestore.instance
+      .collection("users")
+      .doc(FirebaseAuth.instance.currentUser!.email)
+      .collection("liked_quotes")
+    .get().timeout(Duration(seconds: 5)).then((QuerySnapshot querySnapshot) {
+        for (DocumentSnapshot doc in querySnapshot.docs) {
+          likedQuotes.add(doc.id);
+        }
+    }).catchError((firestoreError) {
+      error = firestoreErrorHandler(log, firestoreError);
+    });
+
+    return (error, likedQuotes);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +47,21 @@ class _MainPageState extends State<MainPage> {
         child: IndexedStack(
           index: currentPageIndex,
           children: [
-            ExplorePage(),
+            FutureBuilder(
+              future: getLikedQuotes(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError || (snapshot.hasData && snapshot.data!.$1 != null)) {
+                  debugPrint("Unknown flutter error getting liked quotes: ${snapshot.error}");
+                  return Text("An unknown error occurred");
+                }
+                else if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                else {
+                  return ExplorePage(snapshot.data!.$2);
+                }
+              }
+            ),
             Scaffold(
               appBar: AppBar(
                 title: Text("Settings"),
