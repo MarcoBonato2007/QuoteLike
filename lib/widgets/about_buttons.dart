3e8dc9cc7e14aa4bf96fdb4bb0100a7fea794921
MarcoBonato2007 <MarcoBonato2007@gmail.com>
2025-09-20 12:01:41 +0200

    Full privacy policy addition

---
+++ lib/widgets/about_buttons.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:quotelike/utilities/globals.dart';
+import 'package:quotelike/widgets/standard_widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PrivacyPolicyButton extends StatelessWidget {
+  const PrivacyPolicyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StandardSettingsButton("Privacy policy", Icon(Icons.privacy_tip), () async {
+      final url = Uri.parse("https://github.com/MarcoBonato2007/QuoteLike/blob/main/PRIVACY_POLICY.md");
+      if (!await launchUrl(url) && context.mounted) {
+        showToast(
+          context, 
+          "test",
+          Duration(seconds: 3)
+        );
+      }
+    });
+  }
+}
+
+class AboutButton extends StatelessWidget {
+  const AboutButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StandardSettingsButton("About", Icon(Icons.info), () async {
+      if (context.mounted) {
+        showAboutDialog(
+          context: context,
+          applicationName: "QuoteLike",
+          applicationIcon: Icon(Icons.format_quote),
+          children: [Text("QuoteLike is an app where you can scroll a collection of quotes, and find those you like.")],
+          applicationVersion: (await PackageInfo.fromPlatform()).version,
+        );            
+      }
+    });
+  }
+}
